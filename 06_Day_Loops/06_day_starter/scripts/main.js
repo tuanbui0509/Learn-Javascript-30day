a12eeff6b3a4ddff876d014c34d54adacea25145
buingoctuan99 <buingoctuan99@gmail.com>
2021-03-14 16:25:47 +0700

    Update Day 6 Loop

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,2 +1,11 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const myInfo = {
+    name: 'Ngọc Tuấn',
+    age: 22,
+    date: '05/09/1999'
+}
+
+console.log(Object.keys(myInfo));
+
+for (const i of Object.keys(myInfo)) {
+    console.log(myInfo[i])
+}
