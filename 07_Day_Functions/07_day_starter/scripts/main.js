9f41e116636abb1c5b18f68a3d165654819a4877
tuanbui0509 <tuanbui0509@gmail.com>
2021-03-16 20:52:15 +0700

    Update main.js Day 7

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,2 +1,58 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const courses = [
+  {
+    id: 1,
+    name: 'HTML',
+    coin: {}
+  }, {
+    id: 2,
+    name: 'CSS',
+    coin: 200
+  }, {
+    id: 3,
+    name: 'JS',
+    coin: 300
+  }, {
+    id: 4,
+    name: 'VueJS',
+    coin: 400
+  }, {
+    id: 5,
+    name: 'ReactJS',
+    coin: '{}'
+  }
+]
+
+let i = 0;
+var totalCoin = courses.reduce((acc, curr) => {
+  if (Number(curr.coin))
+    return acc + curr.coin
+  else return acc
+}, 0);
+
+console.log(totalCoin);
+i = 0;
+
+totalCoin = courses.reduce((accumulator, currentValue, currentIndex, originArr) => {
+
+  i++;
+
+  console.table({
+
+    'Lượt chạy: ': i,
+
+    'Biến lưu trữ: ': accumulator,
+
+    'Giá hiện tại: ': currentValue.coin,
+
+    'Tổng tiền: ': accumulator + currentValue.coin
+
+  })
+
+  return accumulator + currentValue.coin;
+
+}, 0);
+
+console.log(totalCoin);
+
+var title = ['html', 'css', 'js']
+console.log(title.includes('html', -3));
